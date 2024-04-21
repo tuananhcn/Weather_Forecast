b2b5b8435a29a7343ddfb0d723e50b36db4240a7
tuananhcn <nguyenanhbee12@gmail.com>
2024-04-21 23:16:25 +0700

    Fixed favorites

---
+++ app/src/main/java/com/example/weatherappj/ChartActivity.java
@@ -0,0 +1,4 @@
+package com.example.weatherappj;
+
+public class ChartActivity {
+}
--- app/src/main/java/com/example/weatherappj/MainActivity.java
+++ app/src/main/java/com/example/weatherappj/MainActivity.java
@@ -475,20 +475,20 @@ public class MainActivity extends AppCompatActivity {
             favoriteIcon.setImageResource(R.drawable.favorite_icon);
         }
     }
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        // Handle the intent from FavoriteWeatherActivity
-//        Intent intent = getIntent();
-//        if (intent != null && intent.hasExtra("CITY_NAME")) {
-//            String city = intent.getStringExtra("CITY_NAME");
-//            if (city != null && !city.isEmpty()) {
-//                cityNameTV.setText(city);
-//                getWeatherInfo(city); // Perform the search
-//                updateFavoriteIcon(city, favoriteIV); // Update the favorite icon
-//            }
-//        }
-//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Handle the intent from FavoriteWeatherActivity
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("CITY_NAME")) {
+            String city = intent.getStringExtra("CITY_NAME");
+            if (city != null && !city.isEmpty()) {
+                cityNameTV.setText(city);
+                getWeatherInfo(city); // Perform the search
+                updateFavoriteIcon(city, favoriteIV); // Update the favorite icon
+            }
+        }
+    }
 private void refreshFavoritesAndUI() {
     // Always get the latest set of favorites from SharedPreferences
     prefs = getSharedPreferences("FAVORITES", MODE_PRIVATE);
